1106817b27a9d15f23675e9f2b77c801b45ada6d
victor <vicopem@gmail.com>
2025-05-28 16:36:19 -0400

    Fixes

---
+++ constants/constants.ts
@@ -0,0 +1,26 @@
+import Instagram from "@/public/img/icons/instagram.svg";
+import Tiktok from "@/public/img/icons/tiktok.svg";
+import X from "@/public/img/icons/x.svg";
+
+export const SOCIALS = [
+  {
+    icon: Instagram,
+    text: "Instagram",
+    link: "https://www.instagram.com/aceandaugust/",
+  },
+  {
+    icon: Tiktok,
+    text: "TikTok",
+    link: "https://www.tiktok.com/@aceandaugust",
+  },
+  {
+    icon: X,
+    text: "X",
+    link: "https://x.com/AceAndAugust",
+  },
+  {
+    icon: LinkedIn,
+    text: "LinkedIn",
+    link: "https://www.linkedin.com/company/ace-august/",
+  },
+];
